7ccebcd8890cece6dc2cf6f2d0ddd15f81a98e7e
rajat.gupta <rajat.gupta@gaussnetworks.com>
2020-12-25 21:33:32 +0530

    added new question

--- src/Array/next_greater_3.java
+++ src/Array/next_greater_3.java
@@ -1,10 +1,53 @@
 package Array;
+
+import java.util.Arrays;
+
 //https://leetcode.com/problems/next-greater-element-iii/
 public class next_greater_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n=1999999999;
+		System.out.print(nextGreaterElement(n));
 	}
+	
+	public static int nextGreaterElement(int n) {
+	   String num = ""+n;
+	   char[] mov =num.toCharArray();
+	   int itr=mov.length-1;
+	   int break_point=-1;
+	   while(itr>=1) {
+		   if(mov[itr]>mov[itr-1]) {
+			   break_point=itr-1;
+			   break;
+		   }
+		   itr--;
+	   }
+	   if(break_point==-1) {
+		   return -1;
+	   }
+	   int minIndex=break_point+1;
+	   for(int i=break_point+1;i<num.length();i++) {
+		   if(mov[i]>mov[break_point] &&  mov[minIndex]>mov[i] ) {
+			   minIndex=i;
+		   }
+	   }
+	   swap(mov,break_point,minIndex);
+	   Arrays.sort(mov, break_point+1, mov.length);
+	   num =new String(mov);
+	   Long lo = Long.parseLong(num);
+	   if(Integer.MAX_VALUE<lo) {
+		   return -1;
+	   }
+	   int data = Integer.parseInt(num);
+       return data;
+    }
+	
+	static void swap(char ar[], int i, int j)  
+    { 
+        char temp = ar[i]; 
+        ar[i] = ar[j]; 
+        ar[j] = temp; 
+    } 
 
 }
--- src/DP/longest_increasing.java
+++ src/DP/longest_increasing.java
@@ -8,6 +8,8 @@ public class longest_increasing {
 		// TODO Auto-generated method stub
 		int arr[] = {1,2,3,4};
 		System.out.print(longestIncreasing(arr));
+		  System.out.print("Length of LIS is "
+                  + LIS(arr, 0, arr.length, Integer.MIN_VALUE));
 	}
 	
 	static int longestIncreasing(int arr[]) {
@@ -24,5 +26,26 @@ public class longest_increasing {
 		}
 		return max;
 	}
+	public static int LIS(int[] A, int i, int n, int prev)
+    {
+        // Base case: nothing is remaining
+        if (i == n) {
+            return 0;
+        }
+ 
+        // case 1: exclude the current element and process the
+        // remaining elements
+        int excl = LIS(A, i + 1, n, prev);
+ 
+        // case 2: include the current element if it is greater
+        // than previous element in LIS
+        int incl = 0;
+        if (A[i] > prev) {
+            incl = 1 + LIS(A, i + 1, n, A[i]);
+        }
+ 
+        // return maximum of above two choices
+        return Integer.max(incl, excl);
+    }
 
 }
